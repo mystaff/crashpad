00fec3b1b9c09ba0cff78ed2323a548787bcf292
Yucel Albar <yucel@timedoctor.com>
2025-01-30 15:38:31 +0300

    fix(STAFF-141736): The app installed on Spanish user profile is unable to relaunch

--- handler/relaunch_signal.cc
+++ handler/relaunch_signal.cc
@@ -19,8 +19,27 @@
 
 #include "base/logging.h"
 
+namespace {
+#if defined(WIN32)
+std::wstring toStdWString(const std::string& str)
+{
+    std::wstring result;
+    std::vector<wchar_t> wcharArr;
+    int size = MultiByteToWideChar(CP_UTF8, 0, str.data(), -1, NULL, 0);
+    if (size > 0) {
+        wcharArr.resize(size);
+        MultiByteToWideChar(CP_UTF8, 0, str.data(), -1, &wcharArr[0], wcharArr.size());
+        result = std::wstring(&wcharArr[0]);
+    }
+    return result;
+}
+const std::wstring crashedPidArg(L"--crashed-pid");
+const std::wstring crashedTimeArg(L"--crashed-time");
+#else
 const std::string crashedPidArg("--crashed-pid");
 const std::string crashedTimeArg("--crashed-time");
+#endif
+
 
 bool crashed = false;
 
@@ -65,6 +84,54 @@ std::string getChildProcessList(const std::string& ppid) {
   return result;
 }
 
+#if defined(WIN32)
+std::vector<wchar_t*> getRelaunchArgv(const std::string& argvStr,
+                                   const std::string& pidCrashed,
+                                   bool& maybeCrashLoop) {
+  maybeCrashLoop = false;
+  std::vector<wchar_t*> cargv;
+  auto crashTime = currentTime();
+
+  const auto argvWstr = toStdWString(argvStr);
+  std::wistringstream stream(argvStr);
+  std::wstring arg, lastArg;
+  bool skipNext = false;
+  while (std::getline(stream, arg, L'|')) {
+    if (!arg.empty() && !skipNext) {
+      if (arg.compare(crashedPidArg) == 0 || arg.compare(crashedTimeArg) == 0) {
+        // don't pass the --crashed-pid or --crashed-time from prev. crashes
+        skipNext = true;
+      } else {
+        // passing c_str() pointers directly to execv() was causing arg
+        // value corruption we create a fresh copy here to prevent that
+        wchar_t* carg = new wchar_t[arg.length() + 1];
+        wcsncpy(carg, arg.c_str(), arg.length() + 1);
+        cargv.push_back(carg);
+      }
+    } else {
+      skipNext = false;
+      if (lastArg.compare(crashedTimeArg) == 0) {
+        // current arg is the previous crash time
+        // check if the last crash is very recent to prevent crash+relaunch
+        // loops
+        auto lastCrashTime = std::stoll(arg);
+        auto diff = crashTime - lastCrashTime;
+        if (diff < 15000) {
+          maybeCrashLoop = true;
+        }
+      }
+    }
+    lastArg = arg;
+  }
+}
+void freeRelaunchArgv(std::vector<wchar_t*>& cargv) {
+  for (wchar_t* const arg : cargv) {
+    if (arg) {
+      delete[] arg;
+    }
+  }
+}
+#else
 std::vector<char*> getRelaunchArgv(const std::string& argvStr,
                                    const std::string& pidCrashed,
                                    bool& maybeCrashLoop) {
@@ -139,6 +206,9 @@ void freeRelaunchArgv(std::vector<char*>& cargv) {
     }
   }
 }
+#endif
+
+}  // namespace
 
 namespace td {
 void SetCrashed() {
@@ -166,7 +236,7 @@ void KillChildProcesses(const std::string& pid) {
 
 void RelaunchOnCrash(const std::map<std::string, std::string>& annotations) {
   if (crashed) {
-    auto appPath = annotations.find("__td-relaunch-path");
+    auto appPath = annotations.find(" ");
     auto pidCrashed = annotations.find("__td-crashed-pid");
 
     if (appPath != annotations.end() && pidCrashed != annotations.end()) {
@@ -188,7 +258,10 @@ void RelaunchOnCrash(const std::map<std::string, std::string>& annotations) {
 #if defined(WIN32)
         /*Windows needs some delay to create envelope file to create before restarting the app*/
         Sleep(5000);
-        auto returnC = _execvp(appPath->second.c_str(), cargv.data());
+
+        const auto appPathW = toStdWString(appPath->second);
+        const auto 
+        auto returnC = _wexecvp(appPath->second.c_str(), cargv.data());
 #else
         auto returnC = execvp(appPath->second.c_str(), cargv.data());
 #endif
